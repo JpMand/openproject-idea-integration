40b0520d222a30d945cc7e07867eadb312aeb0d1
Joao Borghetti <joao.borghetti@trilogis.it>
2025-09-08 12:21:28 +0200

    fix test

--- src/main/java/com/github/jpmand/openproject/integration/api/models/OPRoleModel.java
+++ src/main/java/com/github/jpmand/openproject/integration/api/models/OPRoleModel.java
@@ -13,6 +13,9 @@ public class OPRoleModel extends OPBaseResource {
     @JsonProperty("name")
     private String name;
 
+    @JsonProperty("title")
+    private String title;
+
     public OPRoleModel() {
     }
 
@@ -37,12 +40,20 @@ public class OPRoleModel extends OPBaseResource {
         this.name = name;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Override
     public final boolean equals(Object o) {
         if (!(o instanceof OPRoleModel that)) return false;
         if (!super.equals(o)) return false;
 
-        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName());
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName()) &&  Objects.equals(getTitle(), that.getTitle());
     }
 
     @Override
@@ -50,6 +61,7 @@ public class OPRoleModel extends OPBaseResource {
         int result = super.hashCode();
         result = 31 * result + getId().hashCode();
         result = 31 * result + Objects.hashCode(getName());
+        result = 31 * result + Objects.hashCode(getTitle());
         return result;
     }
 
@@ -59,6 +71,7 @@ public class OPRoleModel extends OPBaseResource {
         sb.append("id=").append(getId());
         sb.append(", name='").append(getName()).append('\'');
         sb.append(", type='").append(getType()).append('\'');
+        sb.append(", title='").append(getTitle()).append('\'');
         sb.append(", links=").append(getLinks());
         sb.append('}');
         return sb.toString();
--- src/test/java/com/github/jpmand/openproject/integration/api/models/OPRoleModelTest.java
+++ src/test/java/com/github/jpmand/openproject/integration/api/models/OPRoleModelTest.java
@@ -2,7 +2,9 @@ package com.github.jpmand.openproject.integration.api.models;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.jpmand.openproject.integration.api.models.base.AbstractOPCollection;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,7 +14,7 @@ public class OPRoleModelTest {
     @Test
     public void testOPRoleModelJson() throws JsonProcessingException {
         String json = "{\"_type\":\"Role\",\"id\":13,\"name\":\"Standard global role\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/13\",\"title\":\"Standard global role\"}}}";
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         OPRoleModel model =  mapper.readValue(json, OPRoleModel.class);
         Assert.assertNotNull(model);
         Assert.assertEquals(Integer.valueOf(13), model.getId());
@@ -22,7 +24,7 @@ public class OPRoleModelTest {
     @Test
     public void testOPRoleModelCollectionJson() throws JsonProcessingException {
         String json = "{\"_type\":\"Collection\",\"total\":13,\"count\":13,\"_embedded\":{\"elements\":[{\"_type\":\"Role\",\"id\":13,\"name\":\"Standard global role\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/13\",\"title\":\"Standard global role\"}}},{\"_type\":\"Role\",\"id\":12,\"name\":\"Project query editor\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/12\",\"title\":\"Project query editor\"}}},{\"_type\":\"Role\",\"id\":11,\"name\":\"Project query viewer\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/11\",\"title\":\"Project query viewer\"}}},{\"_type\":\"Role\",\"id\":10,\"name\":\"Work package viewer\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/10\",\"title\":\"Work package viewer\"}}},{\"_type\":\"Role\",\"id\":9,\"name\":\"Work package commenter\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/9\",\"title\":\"Work package commenter\"}}},{\"_type\":\"Role\",\"id\":8,\"name\":\"Work package editor\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/8\",\"title\":\"Work package editor\"}}},{\"_type\":\"Role\",\"id\":7,\"name\":\"Github\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/7\",\"title\":\"Github\"}}},{\"_type\":\"Role\",\"id\":6,\"name\":\"Staff and projects manager\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/6\",\"title\":\"Staff and projects manager\"}}},{\"_type\":\"Role\",\"id\":5,\"name\":\"Reader\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/5\",\"title\":\"Reader\"}}},{\"_type\":\"Role\",\"id\":4,\"name\":\"Member\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/4\",\"title\":\"Member\"}}},{\"_type\":\"Role\",\"id\":3,\"name\":\"Project admin\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/3\",\"title\":\"Project admin\"}}},{\"_type\":\"Role\",\"id\":2,\"name\":\"Anonymous\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/2\",\"title\":\"Anonymous\"}}},{\"_type\":\"Role\",\"id\":1,\"name\":\"Non member\",\"_links\":{\"self\":{\"href\":\"/api/v3/roles/1\",\"title\":\"Non member\"}}}]},\"_links\":{\"self\":{\"href\":\"/api/v3/roles?filters=%5B%5D\"}}}";
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         AbstractOPCollection<OPRoleModel> models =  mapper.readValue(json, new TypeReference<AbstractOPCollection<OPRoleModel>>(){});
         Assert.assertNotNull(models);
         Assert.assertEquals(Integer.valueOf(13), models.getTotal());
